f03eff6423846e8f89b948769721a2b82c46e657
Stephanie Rocha M <srochamarquez@bsd220.org>
2025-10-22 10:42:52 -0500

    Initial Commit

---
+++ Pig/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pig
+//
+//  Created by stephanie rocha marquez on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by stephanie rocha marquez on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
